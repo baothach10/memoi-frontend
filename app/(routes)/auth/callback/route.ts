2e2d4ff74a41f62a6b7e75f39477752a52696bc2
baothach10 <baothach10@gmail.com>
2026-01-05 23:20:12 +0700

    refactor: remove logging

--- app/(routes)/auth/callback/route.ts
+++ app/(routes)/auth/callback/route.ts
@@ -6,20 +6,14 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
 
-  console.log('asdasdasd')
-
   if (code) {
-    console.error('123')
     const supabase = await createServerSupabaseClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-    console.log('first')
     if (!error) {
       const {
         data: { session },
         error,
       } = await supabase.auth.getSession();
-
-      console.log('second')
 
       if (error || !session) {
         console.log("Session missing or error:", error);
@@ -33,12 +27,9 @@ export async function GET(request: Request) {
 
       // save to session or local storage
 
-      console.log('third',user, accessToken);
       return NextResponse.redirect(`${origin}`);
     }
   }
-
-  console.log('fourth')
 
   return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
