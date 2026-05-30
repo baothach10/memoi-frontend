e37d15e7805bb4f895d11216d3b79807620c0745
baothach10 <baothach10@gmail.com>
2026-05-31 01:19:25 +0700

    feat: update text

--- app/(routes)/explore/about-us/page.tsx
+++ app/(routes)/explore/about-us/page.tsx
@@ -20,7 +20,7 @@ export default function AboutUsPage() {
       >
         <div className="relative w-2/3 flex flex-col items-center text-center max-mobile:w-full max-mobile:px-4">
           <div className="relative uppercase leading-[140%] text-2xl laptop:max-desktop:w-2/5 desktop:w-1/4 font-regular max-tablet:w-3/5 max-mobile:text-[18px] max-mobile:leading-[120%] max-mobile:w-4/5">
-            MEMOÍ - for the woman becoming herself
+            MEMOÍ - for the woman you are becoming
           </div>
 
           <div className="relative w-[680px] h-[470px] laptop:max-desktop:w-[780px] laptop:max-desktop:h-[470px] smaller-tablet:max-tablet:w-[488px] smaller-tablet:max-tablet:h-[348px] max-mobile:w-[355px] max-mobile:h-[255px]">
